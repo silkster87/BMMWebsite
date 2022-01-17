7598cdd60ad47846467c7dc4d3bfd6126fa7f379
Silky Ng <silky.ng@gmail.com>
2022-01-17 20:51:46 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var myButton = document.getElementById("topButton");
+
+window.onscroll = function(){
+    scrollFunction();
+}
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        myButton.style.display = "block";
+    }else{
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+/*
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navcol-1')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+*/
+
