9deec2f2d882aa466ad28296dc480481576ed8af
Silky Ng <silky.ng@gmail.com>
2022-01-19 13:50:24 +0000

    removed large bootstrap files and used links instead for library

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,8 @@ function topFunction(){
     document.documentElement.scrollTop = 0;
 }
 
-
+//This part is important because without it the menu doesn't collapse when a menu item is clicked on
+//Also when creating bsCollapse we want the menu initially collapsed rather than expanded so toggle is false
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navcol-1')
 const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false})
