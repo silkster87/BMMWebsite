172bb43def1c842c3145ef36afe734777c813ff9
Silky Ng <silky.ng@gmail.com>
2022-01-19 10:36:07 +0000

    adjusted menu toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,11 @@ function topFunction(){
     document.documentElement.scrollTop = 0;
 }
 
-/*
+
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navcol-1')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false})
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
-*/
 
